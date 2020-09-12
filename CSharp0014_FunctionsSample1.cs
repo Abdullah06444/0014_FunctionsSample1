c952be381a72e63ddf63cef7c6af717fa83cea59
Abdullah06444 <abdullah44gur@gmail.com>
2020-09-12 11:16:45 +0300

    initial commit

---
+++ CSharp0014_FunctionsSample1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp0014_FunctionsSample1
+{
+    class CSharp0014_FunctionsSample1
+    {
+		static int isTriangular(int number) 
+		{
+			int i;
+			for (i = 1; number > 0; i++)
+				number -= i;
+			if (number == 0)
+				return 1;
+			return 0;
+		}
+
+		static void Main(string[] args)
+		{
+
+			int x;
+			do
+            {
+
+				x = int.Parse(Console.ReadLine());
+				if (x > 0)
+					if (Convert.ToBoolean(isTriangular(x)))
+						Console.WriteLine(x + " is a triangular number");
+					else
+						Console.WriteLine(x + " is NOT a triangular number");
+			} while (x > 0);
+
+		}
+	}
+}
